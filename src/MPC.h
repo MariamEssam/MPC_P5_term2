55b1e22aba528cadab7dcf422fb6c5bcf1fcde60
Mariam Essam <mariamessameg@gmail.com>
2018-02-19 20:37:23 +0200

    Test Working Implementation

--- src/MPC.h
+++ src/MPC.h
@@ -6,15 +6,37 @@
 
 using namespace std;
 
-class MPC {
- public:
-  MPC();
+const size_t N = 12;
+const double dt = 0.05;
+const int latency_ind = 2; //latency  in units of dt (100ms)
 
-  virtual ~MPC();
+struct Solution {
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+	vector<double> X;
+	vector<double> Y;
+	vector<double> Psi;
+	vector<double> V;
+	vector<double> Cte;
+	vector<double> EPsi;
+	vector<double> Delta;
+	vector<double> A;
 };
+
+class MPC {
+public:
+	MPC();
+
+	virtual ~MPC();
+
+	// Solve the model given an initial state and polynomial coefficients.
+	// Return the first actuatotions.
+	//vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+	Solution Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+	double delta_prev{ 0 };
+	double a_prev{ 0.1 };
+
+};
+
 
 #endif /* MPC_H */
